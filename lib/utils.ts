fd2bbeb3593eb6be1b805cf94731457afebb6dd3
Sharique-sid <sharique.sid79@gmail.com>
2025-10-08 13:48:58 +0530

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-// Updated: October 7, 2025 - Panna.ai v2.0.0
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
